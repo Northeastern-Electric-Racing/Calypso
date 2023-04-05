d3b77e27eb172538e5489aeb805167e684cd4f43
Peyton-McKee <mckee.p@northeastern.edu>
2023-04-04 23:24:57 -0400

    #12 Transpose Matrix

--- decode_data.py
+++ decode_data.py
@@ -209,7 +209,7 @@ def decode19(data: List[int]) -> Dict[int, Any]:
 
 def decode20(data: List[int]) -> Dict[int, Any]:
     decoded_data = pd.defaultDecode(data)
-    matrixData = np.mat(decoded_data[0:3])
+    matrixData = np.matrix.transpose(np.mat(decoded_data[0:3]))
     transformMatrix = np.mat([[1, 0, 0], [0, np.cos(np.deg2rad(20)), np.sin(np.deg2rad(20))], [0, -np.sin(np.deg2rad(20)), np.cos(np.deg2rad(20))]])
     transformedData = transformMatrix * matrixData
     return {
