52749af6421597b5c3683519d9ec8c2bfbeb6066
Peyton-McKee <mckee.p@northeastern.edu>
2024-02-22 23:11:36 -0500

    prettier

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -30,10 +30,7 @@ impl Client for MqttClient {
         let topic = data.topic.to_string();
         let mut payload = serverdata::ServerData::new();
         payload.unit = data.unit.to_string();
-        payload.value = data.value
-                            .iter()
-                            .map(|x| x.to_string())
-                            .collect();
+        payload.value = data.value.iter().map(|x| x.to_string()).collect();
 
         /* If the client is initialized, publish the data. */
         if let Some(client) = &self.client {
