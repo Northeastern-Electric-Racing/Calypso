8a8d36dfbde05142784129459908a7fed4623a78
tszwingli <43227796+tszwinglitw@users.noreply.github.com>
2024-10-06 15:27:53 -0400

    improved new value generation

--- src/simulatable_message.rs
+++ src/simulatable_message.rs
@@ -1,6 +1,6 @@
-use std::time::Instant;
 use super::data::DecodeData;
 use rand::prelude::*;
+use std::time::Instant;
 
 /**
  * Wrapper class for an individual message.
@@ -11,7 +11,7 @@ pub struct SimulatedComponents {
     pub unit: String,         // DecodeData.unit
     pub last_update: Instant, // when the data was last updated
     pub n_canpoints: u32,     // number of can points
-    pub sim_min: f32,         // min value 
+    pub sim_min: f32,         // min value
     pub sim_max: f32,         // max value
     pub sim_inc_min: f32,     // min increment step
     pub sim_inc_max: f32,     // max increment step
@@ -30,12 +30,12 @@ impl SimulatedComponents {
     }
 
     pub fn new(
-        topic: String, 
-        unit: String, 
-        sim_min: f32, 
-        sim_max: f32, 
-        sim_inc_min: f32, 
-        sim_inc_max: f32, 
+        topic: String,
+        unit: String,
+        sim_min: f32,
+        sim_max: f32,
+        sim_inc_min: f32,
+        sim_inc_max: f32,
         sim_freq: f32,
         n_canpoints: u32,
         format: String,
@@ -45,7 +45,7 @@ impl SimulatedComponents {
         println!("[SimulatedComponents.initialize] Initializing simulated components");
 
         let mut value = vec![0.0; n_canpoints as usize];
-        
+
         // initialize value with random values between sim_min and sim_max
         let mut rng = rand::thread_rng();
         for i in 0..n_canpoints as usize {
@@ -69,17 +69,34 @@ impl SimulatedComponents {
         }
     }
 
-
     pub fn update(&mut self) {
         println!("[SimulatedComponents.update] Updating simulated components");
+        println!("[SimulatedComponents.update] id: {}, format: {}, sim_min: {}, sim_max: {}, sim_inc_min: {}, sim_inc_max: {}, sim_freq: {}", self.id, self.format, self.sim_min, self.sim_max, self.sim_inc_min, self.sim_inc_max, self.sim_freq);
         if self.should_update() {
             self.last_update = Instant::now();
             let mut rng = rand::thread_rng();
             for i in 0..self.value.len() {
-                let rand_offset = rng.gen_range(self.sim_inc_min..self.sim_inc_max);
+                let mut rand_offset: f32;
+
+                // handle the case where sim_inc_min == sim_inc_max
+                if self.sim_inc_min == self.sim_inc_max {
+                    rand_offset = self.sim_inc_min;
+                } else {
+                    rand_offset = rng.gen_range(self.sim_inc_min..self.sim_inc_max);
+                }
                 let sign = if rng.gen_bool(0.5) { 1.0 } else { -1.0 };
-                let new_value = self.value[i] + sign * rand_offset;
-                // TODO: add handling of exceeding sim_min and sim_max
+                
+                // getting a in-range new value
+                let mut new_value = self.value[i] + sign * rand_offset;
+                while new_value < self.sim_min || new_value > self.sim_max {
+                    if self.sim_inc_min == self.sim_inc_max {
+                        rand_offset = self.sim_inc_min;
+                    } else {
+                        rand_offset = rng.gen_range(self.sim_inc_min..self.sim_inc_max);
+                    }
+                    let sign = if rng.gen_bool(0.5) { 1.0 } else { -1.0 };
+                    new_value = self.value[i] + sign * rand_offset;
+                }
                 self.value[i] = new_value;
             }
         }
