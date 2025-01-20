ca9ffb45733021690bd585984a2cdc70351c4c2a
Jack Rubacha <rubacha.jack03@gmail.com>
2025-01-19 23:50:26 -0500

    Make not require linux for socketcan (#93)
    * changes to deps, docker stuff
    
    * remove protobuf from readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-pub mod command_data;
 pub mod data;
 pub mod decode_data;
 pub mod encode_data;
 pub mod mqtt;
-pub mod serverdata;
+pub mod proto;
 pub mod simulatable_message;
 pub mod simulate_data;
